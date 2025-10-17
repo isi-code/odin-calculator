6dea5a6b28e941db3e2ea46287159e26b9893208
isi-code <irvinjose.silva@gmail.com>
2025-10-17 15:12:09 -0700

    Turn global variables to object properties

--- calculator.js
+++ calculator.js
@@ -1,94 +1,99 @@
-const calculator = {
-    subtract(num1,num2){
-        return num1 - num2
+const calc = {
+    num1 : "",
+    operation : "",
+    operatorSign : "",
+    num2 : "",
+    result : 0,
+    num1Set : false,
+    subtract(){
+        this.operate()
+        this.result = this.num1 - this.num2
     },
-    sum(num1,num2){
-        return num1 + num2
+    sum(){
+        this.operate()
+        this.result = this.num1 + this.num2
+        this.result.toFixed(2)
     },
-    multiply(num1,num2){  
-        return num1 * num2 
+    multiply(){  
+        this.operate()
+        this.result = this.num1 * this.num2 
     },
-    divide(num1,num2){
-        return num1 / num2
+    divide(){
+        this.operate()
+        this.result = this.num1 / this.num2
     },
-    operate(num1,num2,calculate){
-        const result = calculate(num1,num2);
-        return result
+    operate(){
+        this.num1 = this.num1.indexOf('.') === 1 ? parseFloat(this.num1) : parseInt(this.num1);
+        this.num2 = this.num2.indexOf('.') === 1 ? parseFloat(this.num2) : parseInt(this.num2);
     },
 }
 
 const buttons = document.querySelector(".btns-container");
 const display = document.getElementById("display");
 
-let num1 = "";
-let operation= "";
-let operatorSign = "";
-let num2 = "";
-let result = 0;
-let num1Set = false;
-
 buttons.addEventListener("click",(e)=>{
     if (e.target.tagName === "BUTTON"){        
         switch(e.target.value) {
             case "-":
-                operation = "subtract";
-                operatorSign = e.target.value;
+                calc.operation = "subtract";
+                calc.operatorSign = e.target.value;
             break;
             case "+":
-                operation = "sum"; 
-                operatorSign += e.target.value;
+                calc.operation = "sum"; 
+                calc.operatorSign += e.target.value;
             break;
             case "*":
-                operation = "multiply";
-                operatorSign = e.target.value;
+                calc.operation = "multiply";
+                calc.operatorSign = e.target.value;
             break;
             case "/":
-                operation = "divide";
-                operatorSign = e.target.value;
+                calc.operation = "divide";
+                calc.operatorSign = e.target.value;
             break;
             case "C":
-                num1 = "";
-                num2 = "";
-                operatorSign = "";
+                calc.num1 = "";
+                calc.num2 = "";
+                calc.operatorSign = "";
                 display.textContent = "";
             break;
             case "backspace":
-                if (!num1Set){
-                    num1 = num1.slice(0,-1);
+                if (!calc.num1Set){
+                    calc.num1 = calc.num1.slice(0,-1);
                 }
                 else{
-                    num2 = num2.slice(0,-1);
+                    calc.num2 = calc.num2.slice(0,-1);
                 }
                 break;
             case ".":
-                if (!num1Set && num1.indexOf('.') !== 1){
-                    num1 += e.target.value;
+                if (!calc.num1Set && calc.num1.indexOf('.') !== 1){
+                    calc.num1 += e.target.value;
                 }
-                else if (num2.indexOf('.') !== 1){
-                    num2 += e.target.value;
+                else if (calc.num2.indexOf('.') !== 1 && calc.operation !== ""){
+                    calc.num2 += e.target.value;
                 }
                 break;
             case "=":
-                if (num1 !== "" && num2 !== "" && operation !== "" && operatorSign !== ""){
-                    result = calculator.operate(num1,num2,calculator[operation]);
+                if (calc.num1 !== "" && calc.num2 !== "" && calc.operation !== ""){
+                    calc[calc.operation]()
                 }
                 break;
             default:
-                num1Set = num1 !== "" && operation !== "";
-                if (!num1Set){
-                    num1 += e.target.value;
+                calc.num1Set = calc.num1 !== "" && calc.operation !== "";
+                if (!calc.num1Set){
+                    calc.num1 += e.target.value;
                 } else{
-                    num2 += e.target.value;
+                    calc.num2 += e.target.value;
                 }
         }
-        if (result){ 
-            display.textContent = `${num1} ${operatorSign} ${num2} = ${result}`;
-            num1 = result;
-            result = 0;
-            operation = "";
-            operatorSign = "";
+        if (calc.result){ 
+            display.textContent = `${calc.num1} ${calc.operatorSign} ${calc.num2} = ${calc.result}`;
+            calc.num1 = calc.result.toString();
+            calc.result = 0;
+            calc.operation = "";
+            calc.operatorSign = "";
+            calc.num2 = "";
         } else{
-            display.textContent = `${num1} ${operatorSign} ${num2}`;
+            display.textContent = `${calc.num1} ${calc.operatorSign} ${calc.num2}`;
         }
     }
 });
