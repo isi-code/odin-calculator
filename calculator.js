d9f2901ad2b7803e256398d8e650f8be2b651e1f
isi-code <irvinjose.silva@gmail.com>
2025-10-17 13:46:20 -0700

    Fix clear button and Add float number feature

--- calculator.js
+++ calculator.js
@@ -11,8 +11,8 @@ const calculator = {
     divide(num1,num2){
         return num1 / num2
     },
-    operate(num1,num2,operation){
-        const result = operation(num1,num2);
+    operate(num1,num2,calculate){
+        const result = calculate(num1,num2);
         return result
     },
 }
@@ -24,6 +24,7 @@ let num1 = "";
 let operation= "";
 let operatorSign = "";
 let num2 = "";
+let result = 0;
 let num1Set = false;
 
 buttons.addEventListener("click",(e)=>{
@@ -32,61 +33,62 @@ buttons.addEventListener("click",(e)=>{
             case "-":
                 operation = "subtract";
                 operatorSign = e.target.value;
-                display.textContent = num1 + e.target.value + num2;
             break;
             case "+":
                 operation = "sum"; 
                 operatorSign += e.target.value;
-                display.textContent = num1 + e.target.value + num2;
             break;
             case "*":
                 operation = "multiply";
                 operatorSign = e.target.value;
-                display.textContent = num1 + e.target.value + num2;
             break;
             case "/":
                 operation = "divide";
                 operatorSign = e.target.value;
-                operation, display.textContent = num1 + e.target.value + num2;
             break;
             case "C":
-                num1, num2, operatorSign = "";
+                num1 = "";
+                num2 = "";
+                operatorSign = "";
                 display.textContent = "";
             break;
             case "backspace":
                 if (!num1Set){
                     num1 = num1.slice(0,-1);
-                    display.textContent = num1;
                 }
                 else{
                     num2 = num2.slice(0,-1);
-                    display.textContent = num2;
                 }
                 break;
-            // case ".":
-            //     if (!num1Set && num1.indexOf('.') !== 1){
-            //         num1 += e.target.value;
-            //     }
-            //     else if (num2.indexOf('.') !== 1){
-            //         num2 += e.target.value;
-            //     }
-            // break;
+            case ".":
+                if (!num1Set && num1.indexOf('.') !== 1){
+                    num1 += e.target.value;
+                }
+                else if (num2.indexOf('.') !== 1){
+                    num2 += e.target.value;
+                }
+                break;
             case "=":
-                if (num1 !== "" && num2 !== "" && operatorSign !== "" ){
-                    let result = calculator.operate(num1,num2,calculator[operation]);
-                    display.textContent = `${num1} + ${num2} = ${result}`;
-                    num1 = result;
+                if (num1 !== "" && num2 !== "" && operation !== "" && operatorSign !== ""){
+                    result = calculator.operate(num1,num2,calculator[operation]);
                 }
                 break;
             default:
                 num1Set = num1 !== "" && operation !== "";
                 if (!num1Set){
                     num1 += e.target.value;
-                    display.textContent = num1 + operatorSign;
                 } else{
                     num2 += e.target.value;
-                    display.textContent = num1 + operatorSign + num2;
                 }
+        }
+        if (result){ 
+            display.textContent = `${num1} ${operatorSign} ${num2} = ${result}`;
+            num1 = result;
+            result = 0;
+            operation = "";
+            operatorSign = "";
+        } else{
+            display.textContent = `${num1} ${operatorSign} ${num2}`;
         }
     }
 });
