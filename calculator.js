b0c87eb1586635bf87bcc90d5b8fa6b583065933
isi-code <irvinjose.silva@gmail.com>
2025-10-17 11:19:36 -0700

    Add operation methods to calculator object
    *Comments to styling added*

--- calculator.js
+++ calculator.js
@@ -1,24 +1,25 @@
-function sum(){
-
-}
-
-
-function multiply(){
-
+calculator = {
+    substract(num1,num2){
+        return num1 - num2
+    },
+    sum(num1,num2){
+        return num1 + num2
+    },
     
+    multiply(num1,num2){  
+        return num1 * num2 
+    },
+    divide(num1,num2){
+        return num1 / num2
+    },
+    operate(num1,num2,operation){
+        const result = operation(num1,num2);
+        return result
+    },
+    cBtn(display){
+
+    },
+    backspace(display){
+
+    }
 }
-
-
-function divide(){
-
-}
-
-function substract(){
-
-}
-
-function operate(){
-
-}
-
-
