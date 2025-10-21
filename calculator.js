2fb7c6e893372f6d18a56789a061af09758fc738
isi-code <irvinjose.silva@gmail.com>
2025-10-21 11:55:45 -0700

    Changed how numbers are set

--- calculator.js
+++ calculator.js
@@ -1,8 +1,6 @@
 const calc = {
     num1 : "",
     num2:"",
-    num1IsSet : false,
-    num2IsSet : false,    
     operation : "",
     subtract(num1,num2){
         return num1 - num2;
@@ -19,13 +17,20 @@ const calc = {
             return division;
         }
         else{
-            return "Error";
+            return "You can't divide by 0";
         }
     },
     operate(num1,num2,operator){
         let result = this[operator](num1,num2);
-        if (result !== 0 && result !== "Error"){
-            result = Number.isInteger(result) ? result : result.toFixed(2); 
+        if (typeof result !== "string" ){
+            this.num1 = result;
+            this.num2 = "";
+            result = Number.isInteger(result) ? result : result.toFixed(2);
+        }
+        else{
+            this.num1 = "";
+            this.num2 = "";
+            this.operation = "";
         }
         return result
     },
@@ -37,37 +42,45 @@ const calc = {
 const buttons = document.querySelector(".btns-container");
 const display = document.getElementById("display");
 
-buttons.addEventListener("click",(e)=>{
+buttons.addEventListener("click",(e) => {
     if (e.target.tagName === "BUTTON"){    
         switch(e.target.value) {
             case "subtract":
-                if (calc.operation !== "" && calc.num2 !== "" && typeof calc.num2 === "string") {
+                if(calc.operation === "") calc.operation = e.target.value;
+                else{
                     calc.num2 = calc.num2.includes('.') ? parseFloat(calc.num2) : parseInt(calc.num2);
-                } else{
-                calc.operation = e.target.value;                
+                    let result = calc.operate(calc.num1,calc.num2,calc.operation)
+                    calc.operation = e.target.value;
+                    calc.display(display,result)
                 }
-            break;
+                break;
             case "sum":
-                if (calc.operation !== "" && calc.num2 !== "" && typeof calc.num2 === "string") {
+                if(calc.operation === "") calc.operation = e.target.value;
+                else{
                     calc.num2 = calc.num2.includes('.') ? parseFloat(calc.num2) : parseInt(calc.num2);
-                } else{
-                calc.operation = e.target.value;                
+                    let result = calc.operate(calc.num1,calc.num2,calc.operation)
+                    calc.operation = e.target.value;
+                    calc.display(display,result)
                 }
-            break;
+                break;
             case "multiply":
-                if (calc.operation !== "" && calc.num2 !== "" && typeof calc.num2 === "string") {
+                if(calc.operation === "") calc.operation = e.target.value;
+                else{
                     calc.num2 = calc.num2.includes('.') ? parseFloat(calc.num2) : parseInt(calc.num2);
-                } else{
-                calc.operation = e.target.value;                
+                    let result = calc.operate(calc.num1,calc.num2,calc.operation)
+                    calc.operation = e.target.value;
+                    calc.display(display,result)
                 }
-            break;
+                break;
             case "divide":
-                if (calc.operation !== "" && calc.num2 !== "" && typeof calc.num2 === "string") {
+                if(calc.operation === "") calc.operation = e.target.value;
+                else{
                     calc.num2 = calc.num2.includes('.') ? parseFloat(calc.num2) : parseInt(calc.num2);
-                } else{
-                calc.operation = e.target.value;                
+                    let result = calc.operate(calc.num1,calc.num2,calc.operation)
+                    calc.operation = e.target.value;
+                    calc.display(display,result)
                 }
-            break;
+                break;
             case "C":
                 calc.num1 = "";
                 calc.num2 = "";
@@ -77,53 +90,50 @@ buttons.addEventListener("click",(e)=>{
                 calc.display(display,"");
             break;
             case "backspace":
-                if (!calc.num1IsSet) {
+                if (typeof calc.num1 === "string") {
                     calc.num1 = calc.num1.slice(0,-1);
                     calc.display(display,calc.num1)
                 }
-                else if (calc.num1IsSet && !calc.num2IsSet) {
+                else if (typeof calc.num2 === "string") {
                     calc.num2 = calc.num2.slice(0,-1);
                     calc.display(display,calc.num2)
                 }
                 break;
             case ".":
-                if (!calc.num1IsSet){
+                if (typeof calc.num1 === "string"){
                     if(!calc.num1.includes(".")) calc.num1 += e.target.value;
                     if (calc.num1 === ".") calc.num1 = '0'+ calc.num1;
                 }
-                else if (!calc.num2IsSet){
+                else if (typeof calc.num2 === "string"){
                     if(!calc.num2.includes(".")) calc.num2 += e.target.value;
                     if (calc.num2 === ".") calc.num2 = '0'+ calc.num2;
                 }
                 break;
             case "=":
-                if (typeof calc.num2 === "string") {
+                if (calc.num2 !== ""){
                     calc.num2 = calc.num2.includes('.') ? parseFloat(calc.num2) : parseInt(calc.num2);
+                    let result = calc.operate(calc.num1,calc.num2,calc.operation)
+                    calc.display(display,result)
                 }
+                else{
+                    calc.display(display,"Error")
+                }
+                calc.num1 = "";
+                calc.operation = "";
                 break;
             default:
-                if (!calc.num1IsSet) calc.num1 += e.target.value;
-                else if (!calc.num2IsSet) calc.num2 += e.target.value;
+                if (typeof calc.num1 === "string") {
+                    calc.num1 += e.target.value;
+                    calc.display(display,calc.num1)
+                }
+                else if (typeof calc.num2 === "string") {
+                    calc.num2 += e.target.value;
+                    calc.display(display,calc.num2)
+                }
+            }
         }
-
-        if(calc.operation !== "" && calc.num1 !== "" && typeof calc.num1 === "string"){
+        if (calc.num1 !== "" && calc.operation !== ""){
             calc.num1 = calc.num1.includes('.') ? parseFloat(calc.num1) : parseInt(calc.num1);
         }
-
-        if (!calc.num1IsSet || calc.num2 === "") calc.display(display,calc.num1)
-        else if (!calc.num2IsSet) calc.display(display,calc.num2)
-
-        calc.num1IsSet = calc.num1 !== "" && typeof calc.num1 === "number";
-        calc.num2IsSet = calc.num1IsSet && calc.num2 !== "" && typeof calc.num2 === "number";
-
-        if (calc.num1IsSet && calc.num2IsSet){
-            let result = calc.operate(calc.num1,calc.num2,calc.operation);
-            calc.display(display,result);
-            calc.num1 = (result === "Error" || e.target.value === "=") ? "" : result;
-            calc.num1IsSet = (result === "Error" || e.target.value === "=") ? false : true;
-            calc.num2 = "";
-            calc.num2IsSet = false;    
-            calc.operation = e.target.value === "=" ? "" : calc.operation;
-        }    
-    } 
-});
+    }
+);
